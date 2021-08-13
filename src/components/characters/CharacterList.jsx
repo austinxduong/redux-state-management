0f2a04f565723278a7bbb69c8f3a6d4d7d6ae70b
austinxduong <axduong2@gmail.com>
2021-08-13 02:10:09 -0700

    add CharacterList display component

---
+++ src/components/characters/CharacterList.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import Character from './Character';
+
+const CharacterList = () => {
+    const characters = useCharacters();
+
+    const characterElements = characters.map((character) => (
+        <li key={character.Name}>
+            <Character {...character} />
+        </li>
+    ));
+
+    return <ul>{characterElements}</ul>
+    };
+
+export default CharacterList;
