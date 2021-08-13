e400d6602aeb90469c040ecfc1133793f91480c3
austinxduong <axduong2@gmail.com>
2021-08-13 02:21:26 -0700

    adjusted apps.jsx to context provider style

--- src/components/app/App.jsx
+++ src/components/app/App.jsx
@@ -1,11 +1,20 @@
 import React from 'react';
-import { SwitchCase, Route } from 'react-router-dom';
+import { Switch, Route, useParams } from 'react-router-dom';
 import CharacterList from '../characters/CharacterList';
+
+const Home = () => {
+  return <h1>Home page</h1>;
+};
+
+const About = () => {
+  const { Name } = useParams();
+  return <h1>About page {Name} </h1>;
+};
 
 export default function App() {
   return (
-    <Switch>
-      <Route exact path="/" component={CharacterList} />
-    </Switch>
-  )
-};
+    <>
+      <CharacterList />
+    </>
+  );
+}
