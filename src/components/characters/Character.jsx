cf1d2152f68e4ec05909531a8bb7e56b4d17fad4
austinxduong <axduong2@gmail.com>
2021-08-17 16:36:03 -0700

    css

--- src/components/characters/Character.jsx
+++ src/components/characters/Character.jsx
@@ -1,14 +1,17 @@
 /* eslint-disable max-len */
 import React from 'react';
 import PropTypes from 'prop-types';
+import styles from './Character.css';
 
 const Character = ({ name, image }) => (
   <figure>
-    <img src={image} alt={name} />
-    <figcaption>
-      <p>name: {name}</p>
-      {/* <p> placeholder to add more stuff once api connects and renders</p> */}
-    </figcaption>
+    <section className={styles.Character}>
+      <img src={image} alt={name} />
+      <figcaption>
+        <p>Name: {name}</p>
+        {/* <p> placeholder to add more stuff once api connects and renders</p> */}
+      </figcaption>
+    </section>
   </figure>
 );
 
